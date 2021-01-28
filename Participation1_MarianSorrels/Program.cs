8be20cf41fb56b62366310017bf9b07f66128a42
Marian Sorrels <Marian.R.Sorrels-1@ou.edu>
2021-01-28 16:16:10 -0600

    ughhhhh

---
+++ Participation1_MarianSorrels/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+/// <summary>
+/// @Author Marian Sorrels's Project
+/// </summary>
+
+namespace Participation1_MarianSorrels
+{
+    class Program
+    {
+        List<Student> students = new List<Student>();
+        static void Main(string[] args)
+        {
+
+            //absolute reference: full location thing, program can find it wherever it is, even on internet
+            //relative reference: this is like the path where you just put how to find it from one path
+
+            string[] lines = File.ReadAllLines("studentDataRandom100Rows.csv.");
+            // you need to add the thing on line 2 for file
+            //parenthesis means you're using a method
+            //to add your file, you go to the solution explorer, right click on project then add exisitng file
+            //then you go to properties on the imported files and copy to output 
+
+          
+
+
+
+          //  foreach (var line in lines)
+          //  {
+            //    Console.WriteLine(line);
+                //this will print out each line in the lines array
+            //}
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                var pieces = line.Split(',');
+                int id = Convert.ToInt(pieces[0]);
+
+                Student currentStudent =   new Student(Convert.ToInt32)
+            }
+
+            PrintStudentswithFavoriteColor("Blue"); ;
+          //  Student s = new Student();
+          //  s.FirstName = "Joe";
+          //  s.LastName = "Joe";
+            //calls constructor with default values
+          //  Student s2 = new Student(5, "Joe", "Joe", "blue");
+            //calls constructor with data already known
+
+
+        }
+
+        private static void PrintStudentswithFavoriteColor(string color)
+        {
+            throw new NotImplementedException();
+            foreach (var student in students)
+            {
+
+            }
+        }
+    }
+}
+
---
+++ Participation1_MarianSorrels/Student.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Participation1_MarianSorrels
+{
+    public class Student
+    {
+        //read first column of uml
+        // + means public - private
+        //put get and set in curly brackets for public 
+
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FavoriteColor { get; set; }
+
+        //constructers make new rows in the proverbial table
+        //provide starting values for proparties
+        //type ctor is shortcut for constructor
+        //methods need something inside them, even for your empty one
+
+        /// <summary>
+        /// Default/Empty Constructor
+        /// </summary>
+        public Student()
+        {
+            ID = 0;
+            FirstName = "";
+            //you can also say string.Empty;
+            LastName = string.Empty;
+            FavoriteColor = string.Empty;
+        }
+
+        //why is there another constructor? to allow for choice
+        //like making a new row in the table, but with already known values in the cells
+
+        /// <summary>
+        /// overloaded constructor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="first"></param>
+        /// <param name="last"></param>
+        /// <param name="color"></param>
+        public Student(int id, string first, string last, string color)
+        {
+            // the thing you want to change has to be on the left side, you can't put the lowercase stuff on the left side
+            //or you're only changing the lowercase id
+            ID = id;
+            FirstName = first;
+            LastName = last;
+            FavoriteColor = color;
+        }
+
+
+        public Student(int id) : this()
+        {
+            ID = id;
+            //this() is calling back the default/empty constructor
+            //so everything other than ID is gonna be the default empty constructor
+        }
+
+        public string CreateFullName()
+        {
+            //below is easier way to put the values in
+            return $"{LastName}, {FirstName}";
+        }
+
+    }
+}
