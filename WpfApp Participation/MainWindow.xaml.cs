7fcff7e5012c1b12bfd2ab4e793fc81c161b1195
Marian Sorrels <Marian.R.Sorrels-1@ou.edu>
2021-02-02 14:28:15 -0600

    finished this wpf

---
+++ WpfApp Participation/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_Participation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+          //  MessageBox.Show("bananarama");
+
+            MessageBox.Show("You're Welcome!", "You've been blessed", MessageBoxButton.OK);
+            lblMessage.Content = "Hellow World";
+            lblOUtput.Content
+        }
+    }
+}
