b4905f7ec94fedd0dfbb4e0d44b8593320418fd1
Marian Sorrels <Marian.R.Sorrels-1@ou.edu>
2021-02-28 17:23:31 -0600

    sync

---
+++ JSON_Participation_Pokemon/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_Participation_Pokemon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            pokeapi api;
+            string url = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=1200";
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+                api = JsonConvert.DeserializeObject<pokeapi>(json);
+
+            }
+            foreach (var item in api.results.OrderBy(x => x.name).ToList())
+
+            {
+                lstPokemon.Items.Add(item);
+            }
+
+        }
+
+        private void lstPokemon_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ JSON_Participation_Pokemon/pokeapi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_Participation_Pokemon
+{
+    public class pokeapi
+    {
+        public List<ResultObject> results { get; set; }
+
+    }
+
+    public class ResultObject
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return $"{name}";
+        }
+    }
+}
